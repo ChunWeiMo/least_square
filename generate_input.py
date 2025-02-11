c980fececd39399eb73432f92bcc92b290dd336e
chunwei.mo <chunwei.mo@gmail.com>
2025-02-11 12:58:44 -0800

    [FEAT] implement generate Cci_sample

--- generate_input.py
+++ generate_input.py
@@ -1,19 +1,31 @@
 import json
 import numpy as np
 
-def generate_input():
-    pass
+
+def generate_input(*args, **kwargs):
+    if args[0] == "Cci":
+        k_Cci_boundary = args[1]["k_Cci1"]
+        k_Cci_sample = np.linspace(k_Cci_boundary[0], k_Cci_boundary[1], 10)
+
+        phi_Cci_boundary = args[1]["phi_Cci1"]
+        phi_Cci_sample = np.linspace(
+            phi_Cci_boundary[0], phi_Cci_boundary[1], 10)
+        k_Cci, phi_Cci = np.meshgrid(k_Cci_sample, phi_Cci_sample)
+        Cci_sample = np.column_stack((np.ravel(k_Cci), np.ravel(phi_Cci)))
+
+        with open("Cci_sample.csv", "w") as f:
+            f.write("k_Cci,phi_Cci\n")
+            for row in Cci_sample:
+                f.write("{},{}\n".format(row[0], row[1]))
+
 
 def main():
     with open('config_input.json', 'r') as f:
         input_config = json.load(f)
-    
-    k_Cci_min, k_Cci_max = input_config["Cci"]["k_Cci1"]
-    phi_Cci1_min, phi_Cci1_max = input_config["Cci"]["phi_Cci1"]
-    
-    k_Cci_boundary = (k_Cci_min, k_Cci_max)
-    phi_Cci_boundary = (phi_Cci1_min, phi_Cci1_max)
-    generate_input(k_Cci_boundary, phi_Cci_boundary)
-    
+
+    for species, parameters in input_config.items():
+        generate_input(species, parameters)
+
+
 if __name__ == '__main__':
     main()
