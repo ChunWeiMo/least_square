9aa19dbc382a5879000a4bed680dd7a4c7c5ac7a
chunwei.mo <chunwei.mo@gmail.com>
2025-02-14 13:54:02 -0800

    [UPDATE] the numbers of samples loaded from config_input.json

--- generate_input.py
+++ generate_input.py
@@ -3,13 +3,27 @@ import numpy as np
 
 
 def generate_input(*args, **kwargs):
-    if args[0] == "Cci":
+    """
+    Description
+    -----------
+    Generate input for reactions. 
+    
+    Note
+    ----
+    Every parameters has three values:
+        1. Lower boundary
+        2. Upper boundary
+        3. Number of samples
+    """
+    if args[0] == "r_Cci1":
+        print("Generating input for Cci...")
         k_Cci_boundary = args[1]["k_Cci1"]
-        k_Cci_sample = np.linspace(k_Cci_boundary[0], k_Cci_boundary[1], 10)
+        k_Cci_sample = np.linspace(
+            k_Cci_boundary[0], k_Cci_boundary[1], k_Cci_boundary[2])
 
         phi_Cci_boundary = args[1]["phi_Cci1"]
         phi_Cci_sample = np.linspace(
-            phi_Cci_boundary[0], phi_Cci_boundary[1], 10)
+            phi_Cci_boundary[0], phi_Cci_boundary[1], phi_Cci_boundary[2])
         k_Cci, phi_Cci = np.meshgrid(k_Cci_sample, phi_Cci_sample)
         Cci_sample = np.column_stack((np.ravel(k_Cci), np.ravel(phi_Cci)))
 
@@ -21,6 +35,7 @@ def generate_input(*args, **kwargs):
 
 def main():
     with open('config_input.json', 'r') as f:
+        print("Reading input configuration...") 
         input_config = json.load(f)
 
     for species, parameters in input_config.items():
