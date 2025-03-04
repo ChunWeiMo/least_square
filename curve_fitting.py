e0ba135a8b2a0a9c5899f6bf2e3e6d70ddb3a5af
chunwei.mo <chunwei.mo@gmail.com>
2025-03-04 15:59:06 -0800

    [UPDATE] complete polynomial function fitting

--- curve_fitting.py
+++ curve_fitting.py
@@ -2,37 +2,53 @@ import json
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+import numpy as np
 
 
-def func(x, a, b, c, d, e, f):
-    return a*x**4 + b*x**3 + c*x**2 + d*x + e
+def func_poly(x, *coefficients):
+    return sum([c*x**i for i, c in enumerate(coefficients)])
 
 
-def read_experiment_data():
-    with open("plot_curves_setting.json", "r") as f:
-        experiment_data_file = json.load(f)["experiment_data_file"]
+def func_exp(x, a, b, c):
+    return a*np.exp(b*x) + c
 
+
+def calculate_x_axis(array, timestep=60):
+    iteration = len(array)
+    duration = iteration * timestep / 86400
+    x = np.linspace(0, duration, len(array))
+    return x
+
+def sum_of_square_error():
+    pass
+
+def main():
+    with open("curve_fitting_setting.json", "r") as f:
+        setting_json = json.load(f)
+        data_point = setting_json["data_point"]
+        data_number = setting_json["data_numbers_to_fit"]
+        polynomial_degree = setting_json["polynomial_degree"]
+        timestep = setting_json["timestep"]
     try:
-        experiment_data = pd.read_csv(experiment_data_file, header=None)
-        print(experiment_data)
+        data_point = pd.read_csv(data_point, header=None)
     except Exception as e:
         print(f"Error: {e}")
     else:
-        params, _ = curve_fit(func, experiment_data[0], experiment_data[1])
-        a, b, c, d, e = params
-        print(a, b, c, d)
-        X = experiment_data[0]
-        y = func(X, a, b, c, d, e)
-        print(y)
+        step = len(data_point[0]) // data_number
+        x = calculate_x_axis(data_point[0], timestep)
+        
+        x = x[::step]
+        data_point = data_point[0][::step]
+        
+        degree = polynomial_degree
+        initial_guess = [0]+[1]*degree
+        params, _ = curve_fit(func_poly, x, data_point, p0=initial_guess)
+        y = func_poly(x, *params)
 
         fig, ax = plt.subplots()
-        ax.plot(X, y)
-        ax.scatter(experiment_data[0], experiment_data[1], c="red")
+        ax.plot(x, y)
+        ax.scatter(x, data_point, c="red")
         plt.show()
-
-
-def main():
-    pass
 
 
 if __name__ == '__main__':
