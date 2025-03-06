ddcbcdb7a4e8e2321c554cdfe608a14fc73b57f6
chunwei.mo <chunwei.mo@gmail.com>
2025-03-05 16:13:29 -0800

    [REFACTOR] clean up test line and organize code

--- src/sum_of_square_error.py
+++ src/sum_of_square_error.py
@@ -29,51 +29,48 @@ def sum_of_square_error(func, params):
     y = func(x, *params) * 100
 
     sse = np.sum((y - experiment_data[1])**2)
-    # print(f"Sum of square error: {sse}")
-    # fig, ax = plt.subplots()
-    # ax.plot(x, y, label="Fitted curve")
-    # ax.scatter(experiment_data[0], experiment_data[1],
-    #            c="red", label="Experiment")
-    # ax.legend()
-    # plt.show()
     return sse
+
+
+def calculate_all_sse(extraction_matrix, data_number, polynomial_degree, timestep):
+    for extraction in extraction_matrix:
+        iterations = len(extraction["Cu_extraction"])
+        step = iterations // data_number
+        x = calculate_x_axis(iterations, timestep)
+
+        x = x[::step]
+        data_point = extraction["Cu_extraction"][::step]
+
+        degree = polynomial_degree
+        initial_guess = [0] + [1] * degree
+        params, _ = curve_fit(func_poly, x, data_point, p0=initial_guess)
+        y = func_poly(x, *params)
+
+        extraction["sse"] = sum_of_square_error(func_poly, params)
+    df = pd.DataFrame(extraction_matrix)
+
+    fig, ax = plt.subplots()
+    ax.scatter(df["k"], df["sse"])
+    ax.set_xlabel("k")
+    ax.set_ylabel("Sum of square")
+    plt.show()
 
 
 def main():
     with open("../config/curve_fitting.json", "r") as f:
         setting_json = json.load(f)
-        # data_point = setting_json["data_point"]
         data_number = setting_json["data_numbers_to_fit"]
         polynomial_degree = setting_json["polynomial_degree"]
         timestep = setting_json["timestep"]
 
     try:
-        # data_point = pd.read_csv(data_point, header=None)
         extraction_matrix = get_extraction_matrix()
     except Exception as e:
         print(f"Error: {e}")
     else:
-        for extraction in extraction_matrix:
-            iterations = len(extraction["Cu_extraction"])
-            step = iterations // data_number
-            x = calculate_x_axis(iterations, timestep)
+        calculate_all_sse(extraction_matrix, data_number,
+                          polynomial_degree, timestep)
 
-            x = x[::step]
-            data_point = extraction["Cu_extraction"][::step]
-
-            degree = polynomial_degree
-            initial_guess = [0] + [1] * degree
-            params, _ = curve_fit(func_poly, x, data_point, p0=initial_guess)
-            y = func_poly(x, *params)
-
-            extraction["sse"] = sum_of_square_error(func_poly, params)
-        df = pd.DataFrame(extraction_matrix)
-        
-        fig, ax = plt.subplots()
-        ax.scatter(df["k"], df["sse"])
-        ax.set_xlabel("k")
-        ax.set_ylabel("Sum of square")
-        plt.show()        
 
 if __name__ == '__main__':
     main()
