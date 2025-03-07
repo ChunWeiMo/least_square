0c7224ddde21ef839851c733d65b1b6d2266962d
chunwei.mo <chunwei.mo@gmail.com>
2025-03-07 13:18:19 -0800

    [UPDATE] add Bbr relative rate_parameters

--- src/generate_input.py
+++ src/generate_input.py
@@ -2,6 +2,7 @@ import json
 import numpy as np
 import os
 
+
 def generate_input(*args, **kwargs):
     """
     Description
@@ -15,7 +16,7 @@ def generate_input(*args, **kwargs):
         2. Upper boundary
         3. Number of samples
     """
-    if args[0] == "r_Cci1":
+    if args[0] == "r_Cci":
         print("Generating input for Cci...")
         k_Cci_boundary = args[1]["k_Cci1"]
         k_Cci_sample = np.linspace(
@@ -24,18 +25,31 @@ def generate_input(*args, **kwargs):
         phi_Cci_boundary = args[1]["phi_Cci1"]
         phi_Cci_sample = np.linspace(
             phi_Cci_boundary[0], phi_Cci_boundary[1], phi_Cci_boundary[2])
-        k_Cci, phi_Cci = np.meshgrid(k_Cci_sample, phi_Cci_sample)
-        Cci_sample = np.column_stack((np.ravel(k_Cci), np.ravel(phi_Cci)))
+
+        print("Generating input for Bbr...")
+        k_Bbr_boundary = args[1]["k_Bbr"]
+        k_Bbr_sample = np.linspace(
+            k_Bbr_boundary[0], k_Bbr_boundary[1], k_Bbr_boundary[2])
+
+        phi_Bbr_boundary = args[1]["phi_Bbr"]
+        phi_Bbr_sample = np.linspace(
+            phi_Bbr_boundary[0], phi_Bbr_boundary[1], phi_Bbr_boundary[2])
+
+        k_Cci, phi_Cci, k_Bbr, phi_Bbr = np.meshgrid(
+            k_Cci_sample, phi_Cci_sample, k_Bbr_sample, phi_Bbr_sample)
+
+        Cci_sample = np.column_stack(
+            (np.ravel(k_Cci), np.ravel(phi_Cci), np.ravel(k_Bbr), np.ravel(phi_Bbr)))
 
         samples_path = "../data/samples"
         if not os.path.exists(samples_path):
             os.makedirs(samples_path)
         csv_path = os.path.join(samples_path, "Cci_samples.csv")
-        
+
         with open(csv_path, "w") as f:
-            f.write("k_Cci,phi_Cci\n")
+            f.write("k_Cci,phi_Cci,k_Bbr,phi_Bbr\n")
             for row in Cci_sample:
-                f.write("{},{}\n".format(row[0], row[1]))
+                f.write("{},{},{},{}\n".format(row[0], row[1], row[2], row[3]))
             print(f"Samples saved to {os.path.abspath(csv_path)}")
 
 
@@ -49,6 +63,7 @@ def main():
         return
 
     for species, parameters in input_config.items():
+        print(species, parameters)
         generate_input(species, parameters)
 
 
