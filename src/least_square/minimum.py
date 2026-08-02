deaa5a328f256bc44407a4921a44a48276dd83fb
ChunWeiMo <chunwei.mo@gmail.com>
2026-08-01 22:35:07 -0700

    [fix] fix minimum.py for class ExtractionMap and Run

--- src/least_square/minimum.py
+++ src/least_square/minimum.py
@@ -5,6 +5,7 @@ import pandas as pd
 from least_square.sum_of_square_error import func_poly, curve_fitting_poly
 from matplotlib import pyplot as plt
 from scipy.optimize import minimize
+from pathlib import Path
 
 
 def get_data_point():
@@ -29,13 +30,24 @@ def get_data_point():
         print(f"y: {y}")
         print(f"bounds: {bounds} \n")
 
-    data_file = os.path.abspath(data_file)
+    data_file = Path.cwd() / data_file
     try:
         data = pd.read_csv(data_file)
     except FileNotFoundError:
         print(f"{data} not found. Exiting...")
-    else:
-        return data[x], data[y], [bounds,]
+
+    mask_low = bounds[0]
+    mask_high = bounds[1]
+
+    mask = (mask_low <= data[x]) & (data[x] <= mask_high)
+
+    return (
+        data[x][mask],
+        data[y][mask],
+        [
+            bounds,
+        ],
+    )
 
 
 def find_local_minimum(x, y, bounds):
@@ -43,25 +55,26 @@ def find_local_minimum(x, y, bounds):
 
     params = curve_fitting_poly(x, y, 3)
 
-    x_scale = (x-x.min())/(x.max()-x.min())
+    x_scale = (x - x.min()) / (x.max() - x.min())
     scaled_params = curve_fitting_poly(x_scale, y, 3)
-    def objective(x): return func_poly(x, *scaled_params)
+
+    def objective(x):
+        return func_poly(x, *scaled_params)
 
     x0 = np.mean(x_scale)
 
-    scaled_bounds_min = (bounds[0][0]-x.min())/(x.max()-x.min())
-    scaled_bounds_max = (bounds[0][1]-x.min())/(x.max()-x.min())
+    scaled_bounds_min = (bounds[0][0] - x.min()) / (x.max() - x.min())
+    scaled_bounds_max = (bounds[0][1] - x.min()) / (x.max() - x.min())
     scaled_bounds = [[scaled_bounds_min, scaled_bounds_max]]
     minimum = minimize(objective, x0=x0, bounds=scaled_bounds)
 
     if minimum.success:
-        x_min = minimum.x * (x.max()-x.min())+x.min()
+        x_min = minimum.x * (x.max() - x.min()) + x.min()
         print("Local minimum is found:")
         print(f"x= {x_min}")
         plt.scatter(x, y, c="blue", label="model")
         plt.scatter(x, func_poly(x, *params), c="red", label="fitted")
-        plt.scatter(x_min, func_poly(x_min, *params),
-                    c="green", label="minimum")
+        plt.scatter(x_min, func_poly(x_min, *params), c="green", label="minimum")
         plt.legend()
         plt.show()
         return minimum.x
@@ -75,5 +88,5 @@ def main():
         find_local_minimum(x, y, bounds)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
